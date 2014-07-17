3836aa8e33d7869b8b36ff4c64e1a45632c91cde
Yanina Aular (Vauxoo) <yanina.aular@vauxoo.com>
2014-07-17 18:34:48 -0430

    [ADD] Configuration local

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** Ajustes de MySQL. Solicita estos datos a tu proveedor de alojamiento web. ** //
 /** El nombre de tu base de datos de WordPress */
-define('DB_NAME', 'nombredetubasededatos');
+define('DB_NAME', 'wordpress_blog');
 
 /** Tu nombre de usuario de MySQL */
-define('DB_USER', 'nombredeusuario');
+define('DB_USER', 'admin');
 
 /** Tu contraseña de MySQL */
-define('DB_PASSWORD', 'contraseña');
+define('DB_PASSWORD', 'admin');
 
 /** Host de MySQL (es muy probable que no necesites cambiarlo) */
 define('DB_HOST', 'localhost');
